fe2e3cd7229d669dce8088d7e4bf045744e7091e
Jace Ju <jaceju@gmail.com>
2015-02-02 16:38:21 +0800

    Build 0.0.2.

--- src/App/Application.php
+++ src/App/Application.php
@@ -7,7 +7,7 @@ use CLIFramework\Application as CliApp;
 class Application extends CliApp
 {
     const NAME = 'Craftsman';
-    const VERSION = '0.0.1';
+    const VERSION = '0.0.2';
 
     public function options($opts)
     {
