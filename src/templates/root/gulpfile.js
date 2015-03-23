f95dc3943c7b8d645be3a7131d9ec2bd939e9efa
Jace Ju <jaceju@gmail.com>
2015-03-23 15:22:45 +0800

    Replace old gulp tasks with laravel elixir.

--- src/templates/root/gulpfile.js
+++ src/templates/root/gulpfile.js
@@ -1,3 +1,34 @@
 'use strict';
 
-require('require-dir')('./tasks');
+var elixir = require('laravel-elixir');
+require('laravel-elixir-wiredep');
+require('laravel-elixir-useref');
+require('laravel-elixir-browser-sync');
+require('laravel-elixir-serve');
+require('laravel-elixir-jshint');
+require('laravel-elixir-clean');
+require('laravel-elixir-style-guide');
+require('laravel-elixir-apidoc');
+
+elixir(function (mix) {
+    var port = 8000;
+
+    mix.clean()
+        .sass('*.scss')
+        .jshint()
+        .wiredep();
+
+    if (elixir.config.production) {
+        mix.useref({src: false})
+            .version(['js/*.js', 'css/*.css'])
+            .styleGuide()
+            .apidoc();
+    } else {
+        mix.serve({
+            port: port
+        }).browserSync(null, {
+            proxy: 'localhost:' + port,
+            reloadDelay: 2000
+        });
+    }
+});
