e873b0943c0c6a40e44cf70942e2a49ead1cc9d3
Jace Ju <jaceju@gmail.com>
2015-03-26 00:07:20 +0800

    Remove unused files.

--- src/templates/tasks/default.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-
-gulp.task('default', function(cb) {
-    runSequence('clean', 'build', 'hologram', 'apidoc', 'clean:temporary', cb);
-});
